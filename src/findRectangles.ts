efcf19a9f5ae4d1036c95beb0b762cf57ba4cfdd
Suppo94 <46618356+Suppo94@users.noreply.github.com>
2024-12-05 21:03:18 +0200

    Add rectangle finder function

--- src/findRectangles.ts
+++ src/findRectangles.ts
@@ -1,14 +1,14 @@
-function findRectangles(points: Array<[number, number]>): Array<Array<[number, number]>> {
-    const rectangles: Array<Array<[number, number]>> = [];
-    const pointSet = new Set(points.map(point => point.toString()));
-    for (let i = 0; i < points.length; i++) {
-        for (let j = i + 1; j < points.length; j++) {
+export function findRectangles(points: Array&lt;[number, number]&gt;): Array&lt;Array&lt;[number, number]&gt;&gt; {
+    const rectangles: Array&lt;Array&lt;[number, number]&gt;&gt; = [];
+    const pointSet = new Set(points.map(point =&gt; point.toString()));
+    for (let i = 0; i &lt; points.length; i++) {
+        for (let j = i + 1; j &lt; points.length; j++) {
             const [x1, y1] = points[i];
             const [x2, y2] = points[j];
-            if (x1 !== x2 && y1 !== y2) {
+            if (x1 !== x2 &amp;&amp; y1 !== y2) {
                 const point3: [number, number] = [x1, y2];
                 const point4: [number, number] = [x2, y1];
-                if (pointSet.has(point3.toString()) && pointSet.has(point4.toString())) {
+                if (pointSet.has(point3.toString()) &amp;&amp; pointSet.has(point4.toString())) {
                     rectangles.push([points[i], points[j], point3, point4]);
                 }
             }
